d7ce7c9e0c7c29085d2522756ef5292cf2c583d0
Benjamin Kleiner <bizzl@users.sourceforge.net>
2011-07-12 14:43:07 +0200

    Added license headers in php files and COPYING

--- main.php
+++ main.php
@@ -1,17 +1,38 @@
 <?php
-
 /*
-  Plugin Name: WP Easy Help
-  Plugin URI:
-  Description: A help system that integrates into the wordpress admin panel.
-  Version: 1.0.0
-  Author: Benjamin Kleiner
-  Author URI:
-  License: GPLv3
- */
+Plugin Name:	WP Easy Help
+Plugin URI:		http://tacosw.com/htmledit/download.php
+Description:	A help system that integrates into the wordpress admin panel.
+Version:		1.0.0
+Author:			Benjamin Kleiner, Christoph Fritsch
+Author URI:		https://github.com/bizzl-greekdog
+License:		LGPL3
+*/
+/*
+    Copyright (c) 2011 Benjamin Kleiner <bizzl@users.sourceforge.net>
+    Copyright (c) 2011 Christoph Fritsch <christoph@orange-d.net>
+ 
+    This file is part of WP Easy Help.
+
+    WP Easy Help is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    WP Easy Help is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with WP Easy Help. If not, see <http://www.gnu.org/licenses/>.
+*/
 
 if (!function_exists('join_path')) {
 
+	// This is an implementation of pythons sys.path.join()
+	// As the name suggest, join_path takes all arguments and joins
+	// them using the directory separator.
 	function join_path() {
 		$fuck = func_get_args();
 		for ($i = 0; $i < count($fuck); $i++)
@@ -42,6 +63,10 @@ if (!function_exists('mime_content_type')) {
 
 require_once(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'tag.php');
 
+/**
+ * LookUpPath allows us to easily search a file in multiple pathes,
+ * abstracting all that looping hassle.
+ */
 class LookUpPath {
 
 	private $pathes = array();
@@ -72,11 +97,15 @@ class LookUpPath {
 
 }
 
+// A functional interface to LookUpPath.
 function lookup($filename, $pathes) {
 	$lup = new LookUpPath($pathes);
 	return $lup->lookup($filename);
 }
 
+/**
+ * CallbackContext allows for predefined arguments for callback functions.
+ */
 class CallbackContext {
 
 	private $callback = null;
@@ -99,6 +128,10 @@ class CallbackContext {
 
 }
 
+/**
+ * Another abstraction. This one encapsulates both wordpress options interface
+ * and the serialization/deserialization stuff.
+ */
 if (!class_exists('WpOption')) {
 	class WpOption {
 		private $key = '';
@@ -131,6 +164,9 @@ if (!class_exists('WpOption')) {
 	}
 }
 
+/**
+ * Our main class and the actual plugin.
+ */
 class WP_Easy_Help {
 
 	protected static $domain = 'wp-easy-help';
@@ -153,15 +189,18 @@ class WP_Easy_Help {
 		'subscriber' => array()
 	);
 
+	/// Caches the base directory.
 	protected static function init_base() {
 		self::$base = basename(dirname(__FILE__));
 	}
 
+	/// Loads the text domain for gettext.
 	protected static function init_l10n() {
 		$j = join_path(self::$base, 'locale');
 		load_plugin_textdomain(self::$domain, false, $j);
 	}
 
+	/// 
 	public static function init() {
 		self::init_base();
 		self::init_l10n();
@@ -175,6 +214,12 @@ class WP_Easy_Help {
 		add_action('admin_init', array(__CLASS__, 'init_scripts'));
 	}
 	
+	/**
+	 * Loads necessary scripts and styles. See action @admin_init.
+	 * This function also handles the saving of screen options.
+	 *
+	 * Sorry.
+	 */
 	public static function init_scripts() {
 		if (!isset($_REQUEST['entry']))
 			add_filter('screen_settings', array(__CLASS__, 'screen_options'), 10, 2);
@@ -196,6 +241,7 @@ class WP_Easy_Help {
 		}
 	}
 	
+	/// Gets filters for the current users role.
 	public static function get_filters() {
 		foreach (self::$filter_options->get() as $role => $filters)
 			if (current_user_can($role))
@@ -203,6 +249,7 @@ class WP_Easy_Help {
 		return array();
 	}
 
+	/// Prints the inside of the screen options. See filter @screen_settings. 
 	public static function screen_options($current, $screen){
 		if ($screen->parent_file != 'online-help')
 			return $current;
@@ -243,15 +290,21 @@ EOF
 		);
 	}
 
+	/**
+	 * No clue why wp_enqueue_script doesn't like flattr.
+	 * See action @wp_print_scripts.
+	 */
 	public static function print_scripts() {
 		echo script(array('src' => 'http://api.flattr.com/js/0.6/load.js?mode=auto'))->attr('id', 'flattr');
 	}
 
+	/// Adds pages for content and options. See action @admin_menu.
 	public static function menu_init() {
 		add_options_page(__('WP Easy Help', self::$domain), __('WP Easy Help', self::$domain), 'manage_options', 'online-help-settings', array(__CLASS__, 'help_settings'));
 		add_menu_page(__('Help', self::$domain), __('Help', self::$domain), 'edit_posts', 'online-help', array(__CLASS__, 'help'), '', 3);
 	}
 
+	/// Creates a paypal button.
 	public static function do_paypal_button($id) {
 		list($u, $l) = explode('_', get_locale(), 2);
 		return tag('form')
@@ -262,6 +315,7 @@ EOF
 		);
 	}
 
+	/// Creates a flattr button.
 	public static function do_flattr_button($id) {
 		if (!preg_match('/^flattr;/', $id))
 			$id = "flattr;$id";
@@ -273,54 +327,79 @@ EOF
 						))->css('display', 'none')->append('Donate via flattr'))->addClass('donate');
 	}
 
+	/**
+	 * Callback for the /src/-preg_replace. Rebases the src attributes so they
+	 * point to the right files.
+	 */
 	public static function rebase_src($matches, $request_base, $asset_base_path, $asset_base_url) {
-		if (preg_match('#^[a-z0-9]+://#', $matches[1]))
+		if (preg_match('#^[a-z0-9]+://#', $matches[1])) // Keep remote assets.
 			return $matches[0];
-		if (preg_match('#^(\.\./)+assets#', $matches[1]))
+		if (preg_match('#^(\.\./)+assets#', $matches[1])) // The url points to the assets dir, so rebasing is easy.
 			$src = join_path($asset_base_url, 'help', preg_replace('#^(\.\./)+#', '', $matches[1]));
 		else
+			// Looks like we have to search.
 			$src = lookup($matches[1], array(
 				join_path($asset_base_path, 'help', get_locale()),
 				join_path($asset_base_path, 'help', 'en_US'),
 				join_path($asset_base_path, 'help', 'assets')
 					));
 
+		// Prepare the base url so we can turn absolute pathes into absolute urls
 		if ($asset_base_url[strlen($asset_base_url) - 1] != '/')
 			$asset_base_url .= '/';
+			
 		if ($src)
 			$src = str_replace($asset_base_path, $asset_base_url, $src);
 		else
 			$src = 'Whoops, looks like the developer fucked it up!';
+			
 		return "src=\"{$src}\"";
 	}
 
+	/**
+	 * Callback for /href/-preg_replace. Rebases the href attributes so they
+	 * point to the right pages.
+	 */
 	public static function rebase_href($matches, $request_base, $asset_base_path, $asset_base_url) {
-		if (preg_match('#^[a-z0-9]+://#', $matches[1]) || preg_match('/^#/', $matches[1]))
+		if (preg_match('#^[a-z0-9]+://#', $matches[1]) || preg_match('/^#/', $matches[1])) // Keep outgoing and anchor links.
 			return $matches[0];
-		if (preg_match('#^(\.\./)+assets#', $matches[1]))
+			
+		if (preg_match('#^(\.\./)+assets#', $matches[1])) // looks like the url is pointing to an asset.
 			return 'href="' . join_path($asset_base_url, 'help', preg_replace('#^(\.\./)+#', '', $matches[1])) . '" target="_blank"';
+			
 		return "href=\"admin.php?page=online-help&entry={$request_base}/{$matches[1]}\"";
 	}
 
+	/**
+	 * Loads a file and processes it so all src and href attributes
+	 * point to the right places and illegal tags get removed.
+	 */
 	public static function load_file($path, $request_base = 'wordpress', $asset_base_path = '', $asset_base_url = '') {
 		$type = mime_content_type($path);
 		$title = basename($path);
 		$donate = array();
+		
 		if (preg_match('#^text/#', $type)) {
 			$content = file_get_contents($path);
-			if (preg_match('#<title>(.*?)</title>#s', $content, $matches))
+			
+			if (preg_match('#<title>(.*?)</title>#s', $content, $matches)) // Of course we need the files title...
 				$title = $matches[1];
-			if (preg_match_all('#<meta[^>]+>#s', $content, $metas))
+				
+			if (preg_match_all('#<meta[^>]+>#s', $content, $metas)) // ... and some meta tags.
 				foreach ($metas[0] as $meta)
 					if (preg_match('#name="donate"#', $meta) && preg_match('#content="([^"]+)"#', $meta, $matches))
 						$donate[] = $matches[1];
 
+			// Remove all framing tags. We need no head, no body and html tags and doctypes.
 			foreach (array('#</?(body|html)[^>]*>#i', '#<head[^>]*>.*</head>#is', '#<!(?!--)[^>]+>#') as $pattern)
 				$content = preg_replace($pattern, '', $content);
 			
+			// Remove all illegal tags. embeds, objects, links and remote scripts can
+			// introduce trojan horses. Use video and audio for multimedia assets.
 			foreach (array('script[^>]*src=', 'embed', 'object', 'link') as $forbidden)
 				$content = preg_replace("#<{$forbidden}[^>]*>(?:.*?</{$forbidden}[^>]*>)?#i", '', $content);
 
+			// Rebase external urls.
 			$cbx = new CallbackContext(array(__CLASS__, 'rebase_href'), array($request_base, $asset_base_path, $asset_base_url));
 			$content = preg_replace_callback('#href="([^"]+)"#', $cbx->cb(), $content);
 
@@ -339,26 +418,32 @@ EOF
 		return array('title' => $title, 'content' => $content, 'donate' => $donate);
 	}
 
+	/**
+	 * Produces the help page.
+	 */
 	public static function help() {
 		global $current_user;
 		$page = div()->addClass('help');
 		$display_options = self::$display_options->get();
 
-		if (isset($_REQUEST['entry'])) {
+		if (isset($_REQUEST['entry'])) { // The user requested a help page.
+			// Split request into search domain and actual file.
 			list($who, $what) = explode('/', $_REQUEST['entry'], 2);
 			$plugin = array_shift(preg_grep("#{$who}#", self::$plugins));
-			if ($who == 'wordpress') {
+			
+			if ($who == 'wordpress') { // 'wordpress' is for general help.
 				$base_path = get_home_path();
 				$base_url = get_site_url();
-			} elseif ($who == 'you') {
+			} elseif ($who == 'you') { // 'you' is for custom help.
 				$upd = wp_upload_dir();
 				$base_path = $upd['basedir'];
 				$base_url = $upd['baseurl'];
-			} else {
+			} else { // Everything else must be a plugin...
 				$base_path = dirname($plugin);
 				$base_url = plugin_dir_url($plugin);
 			}
 			
+			// The sidebar contains the currents search domains index.
 			$sidebar = div()->addClass('sidebar');
 
 			$file = lookup('index.html', array(
@@ -369,7 +454,7 @@ EOF
 			if ($file) {
 				$file = self::load_file($file, $who, $base_path, $base_url);
 				$sidebar->append(h($file['title'], 4)->addClass('title'), div($file['content'])->addClass('index'));
-				if ($file['donate']) {
+				if ($file['donate']) { // We got donation targets? add necessary buttons.
 					$sidebar->append(h(__('Is this helpful? Then please donate!', self::$domain), 4)->addClass('donate'));
 					foreach ($file['donate'] as $donate) {
 						$donate = preg_split('#: *#', $donate, 2);
@@ -381,6 +466,8 @@ EOF
 				}
 			}
 
+			// Lookup the actually requested file.
+			// Maybe we shouldn't use the functional interface?
 			$file = lookup($what, array(
 				join_path($base_path, 'help', get_locale()),
 				join_path($base_path, 'help', 'en_US'),
@@ -393,11 +480,12 @@ EOF
 				$page->append(div(__('Whoops, looks like this page is missing!', self::$domain))->addClass('content'));
 			}
 			$page->append($sidebar);
-		} else {
+		} else { // Get the main index.
 			$p = self::$display_options->get();
 			$page->addClass('index');
 			$page->append(h(sprintf(__('Welcome %s', self::$domain), $current_user->data->display_name), 1));
 
+			// Lookup and fetch the index of the general help.
 			$index = lookup('index.html', array(
 				join_path(get_home_path(), 'help', get_locale()),
 				join_path(get_home_path(), 'help', 'en_US'),
@@ -413,6 +501,8 @@ EOF
 				$page->append($index);
 			}
 
+			// In case we have a network, we have the possibility of custom help files
+			// for certain blogs.
 			if (is_multisite()) {
 				$upd = wp_upload_dir();
 				$index = lookup('index.html', array(
@@ -431,7 +521,7 @@ EOF
 				}
 			}
 
-
+			// Lookup and fetch the help for all enabled plugins.
 			$plugins_div = div()->attr('id', 'plugin-help');
 			$plugins_div->append(h(__('Plugins', self::$domain), 3));
 			$plugins_div->append($list = tag('ol'));
@@ -460,9 +550,10 @@ EOF
 			$page->append($plugins_div);
 			
 		}
-		echo $page;
+		echo $page; // print the whole shit.
 	}
 	
+	// Prints the help settings, allowing the admin to hide the help for certain plugins.
 	public static function help_settings() {
 		
 		$nonce_field = 'wp-easy-help-settings';
--- tag.class.php
+++ tag.class.php
@@ -1,4 +1,27 @@
 <?php
+/*
+    Copyright (c) 2011 Benjamin Kleiner <bizzl@users.sourceforge.net>
+ 
+    This file is part of Tags.
+
+    Tags is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Tags is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Tags. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*
+God, I hope we'll all use php 5.3 so this can move into a namespace x_X
+And maybe switch to phpquery...
+*/
 
 if (!function_exists('tag')) {
 
